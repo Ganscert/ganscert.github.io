4fedee812a652f39d5ed5fddb09b6d1a82a7170a
Ganscert <emmanuel2929xl@gmail.com>
2024-09-21 03:16:34 -0400

    hide api

--- consumo-API/consumo.js
+++ consumo-API/consumo.js
@@ -1,4 +1,7 @@
-let API_KEY="056f371d92750e3e17d3075f832d4021"
+require('dotenv').config();
+
+const API_KEY = process.env.API_KEY;
+
 let pagina=1
 
 const btnSiguiente=document.getElementById('btnSiguiente')
